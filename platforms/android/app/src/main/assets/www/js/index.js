9db417e7802acbfa6b30bfbf58ce4614238f6ce0
神馬隆博 <takahiro412.c@gmail.com>
2020-02-24 06:55:49 +0900

    ログイン画面を作成

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -31,11 +31,13 @@ function testApiCall() {
   alert('hello');
 }
 
+let LOGIN_AUTH_API_URL = 'http://127.0.0.1:8001/api/login_auth';
+
 $(function(){
     // サーバ接続テストボタン
     $('#test_server_connection_btn').on('click',function() {
         $.ajax({
-            url: 'http://127.0.0.1:8001/api/login_auth',
+            url: LOGIN_AUTH_API_URL,
             type: 'POST',
             data: {'id': 'test_id', 'password': 'password', '_method': 'POST'},
             success: function(response) {
@@ -55,4 +57,46 @@ $(function(){
            },
         });
     });
+
+    // ログインボタン押下
+    $('#login_btn').on('click',function() {
+        var id = $('#id').val();
+        var password = $('#password').val();
+        var err_msg_ele = $('#err_msg').children('p');
+
+        // 入力チェック
+        if(id == "")
+        {
+            err_msg_ele.text("IDを入力してください");
+            return;
+        }
+        if(password == "")
+        {
+            err_msg_ele.text("パスワードを入力してください");
+            return;
+        }
+
+        // サーバ認証
+        $.ajax({
+            url: LOGIN_AUTH_API_URL,
+            type: 'POST',
+            data: {'id': id, 'password': password, '_method': 'POST'},
+            success: function(response) {
+                var json_data = JSON.parse(response);
+                if (json_data.is_login == '1') {
+                    // 認証成功
+                    alert("認証成功");
+                } else {
+                    // 認証失敗
+                    err_msg_ele.text("ID または パスワードが違います");
+                }
+            },
+            fail: function(response) {
+               // ajax失敗時の処理
+               err_msg_ele.text("通信に失敗しました。しばらくしてから再度お試しください。");
+           },
+        });
+    });
+
+
 });
