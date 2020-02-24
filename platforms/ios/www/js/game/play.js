b7f19b125aa904886e23edaaa2c328ed59d2a8aa
神馬隆博 <takahiro412.c@gmail.com>
2020-02-24 17:05:06 +0900

    盤面初期化を実装

--- platforms/ios/www/js/game/play.js
+++ platforms/ios/www/js/game/play.js
@@ -2,7 +2,47 @@ const canvas = $('#canvas-2d')[0];
 const context = canvas.getContext('2d');
 
 $(function(){
+    var board_color = 'rgb(0, 138, 22)';
+    var board_size = 360;
+    var cell_num = 8;
+
     // 指定の色で範囲内を塗りつぶす
-    context.fillStyle = 'rgb(0, 138, 22)';
-    context.fillRect(0, 0, 360, 360);
+    context.fillStyle = board_color;
+    context.fillRect(0, 0, board_size, board_size);
+
+    // 描画処理
+    drawCanvas(cell_num, cell_num, board_size / cell_num, board_color);
 });
+
+function drawCanvas(row_num, col_num, grid_size, color){
+    for(var y = 0; y < row_num; y++){
+            for(var x = 0; x < col_num; x++){
+                    drawGrid(x, y, grid_size, color);
+            }
+    }
+
+    var grid_size_half = grid_size / 2;
+    var stone_ratio = 0.8;
+    drawStone('white', grid_size * 4 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * stone_ratio);
+    drawStone('white', grid_size * 5 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * stone_ratio);
+    drawStone('black', grid_size * 5 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * stone_ratio);
+    drawStone('black', grid_size * 4 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * stone_ratio);
+}
+
+function drawGrid(x, y, grid_size, color){
+    context.clearRect(x * grid_size, y * grid_size, grid_size, grid_size);
+    context.fillStyle = color;
+    context.strokeStyle = 'black';
+    context.fillRect(x * grid_size, y * grid_size, grid_size, grid_size);
+    context.strokeRect(x * grid_size, y * grid_size, grid_size, grid_size);
+}
+
+ function drawStone(color, x, y, radius){
+    context.beginPath();//円を描くためのパスを一度リセットする。
+    context.lineWidth = 0;
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = color;
+    context.fill();
+    context.strokeStyle = color;
+    context.stroke();
+ }
--- www/js/game/play.js
+++ www/js/game/play.js
@@ -1,8 +1,60 @@
 const canvas = $('#canvas-2d')[0];
 const context = canvas.getContext('2d');
+const STONE_RATIO = 0.8;
+
+var board_color;
+var board_size;
+var cell_num;
 
 $(function(){
+    board_color = 'rgb(0, 138, 22)';
+    board_size = 360;
+    cell_num = 8;
+
     // 指定の色で範囲内を塗りつぶす
-    context.fillStyle = 'rgb(0, 138, 22)';
-    context.fillRect(0, 0, 360, 360);
+    context.fillStyle = board_color;
+    context.fillRect(0, 0, board_size, board_size);
+
+    // 描画処理
+    drawCanvas(cell_num, cell_num, board_size / cell_num, board_color);
 });
+
+// 描画処理
+function drawCanvas(row_num, col_num, grid_size, color){
+    initBoard(row_num, col_num, grid_size, color);
+}
+
+// 盤面初期化
+function initBoard(row_num, col_num, grid_size, color){
+    for(var y = 0; y < row_num; y++){
+            for(var x = 0; x < col_num; x++){
+                    drawGrid(x, y, grid_size, color);
+            }
+    }
+
+    var grid_size_half = grid_size / 2;
+    drawStone('white', grid_size * 4 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * STONE_RATIO);
+    drawStone('white', grid_size * 5 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * STONE_RATIO);
+    drawStone('black', grid_size * 5 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * STONE_RATIO);
+    drawStone('black', grid_size * 4 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * STONE_RATIO);
+}
+
+// ボードを描画
+function drawGrid(x, y, grid_size, color){
+    context.clearRect(x * grid_size, y * grid_size, grid_size, grid_size);
+    context.fillStyle = color;
+    context.strokeStyle = 'black';
+    context.fillRect(x * grid_size, y * grid_size, grid_size, grid_size);
+    context.strokeRect(x * grid_size, y * grid_size, grid_size, grid_size);
+}
+
+// 石を描画
+ function drawStone(color, x, y, radius){
+    context.beginPath();//円を描くためのパスを一度リセットする。
+    context.lineWidth = 0;
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = color;
+    context.fill();
+    context.strokeStyle = color;
+    context.stroke();
+ }
