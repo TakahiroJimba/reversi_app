8394ecc240c03efda08fc596af6b425d47d47df5
神馬隆博 <takahiro412.c@gmail.com>
2020-04-04 18:58:20 +0900

    石を置き、ひっくり返すところまで実装

--- www/js/game/play.js
+++ www/js/game/play.js
@@ -1,18 +1,46 @@
 const IS_DEBUG_MODE = true; // デバッグモードで実行する
 
+// APIパス
 const GET_PRIORITY_API_URL = API_URL_BASE + 'api/get_priority';
 const SET_TURN_API_URL     = API_URL_BASE + 'api/set_turn';
 
 const canvas = $('#canvas-2d')[0];
 const context = canvas.getContext('2d');
+
+// 8方向を示すクラス
+class Direction {
+  constructor(d_x, d_y) {
+    this.d_x = d_x;
+    this.d_y = d_y;
+  }
+}
+const direction_array = [];
+
+// マスに対する石の大きさ比率
 const STONE_RATIO = 0.8;
+
+// enum代り
+const NONE = 0;
+const FIRST_COLOR_STONE = 1;
+const SECOND_COLOR_STONE = 2;
+
+// 石の配置を1次元配列で表現
+var board_stone_array = [];
 
 var board_color;
 var board_size;
+var grid_size;
+var grid_size_half;
+
+// セルの数
 var cell_num;
+
 var is_my_turn;
 var user_id;
 var game_id;
+
+// 自分の石の番号(1 or 2)
+var my_stone_number;
 var my_color;
 var first_color;
 var second_color;
@@ -35,15 +63,10 @@ $(function(){
     board_color = 'rgb(0, 138, 22)';
     board_size = 360;
     cell_num = 8;
+    grid_size = board_size / cell_num;
+    grid_size_half = grid_size / 2;
 
     is_my_turn = getPriority(game_id);
-
-    // 指定の色で範囲内を塗りつぶす
-    context.fillStyle = board_color;
-    context.fillRect(0, 0, board_size, board_size);
-
-    // 描画処理
-    drawCanvas(cell_num, cell_num, board_size / cell_num, board_color);
 
     canvas.addEventListener('click', onClick, false);
 });
@@ -60,11 +83,13 @@ function clickFirstOrSecondBtn()
         is_my_turn = true;
         my_color = first_color;
         opponent_color = second_color;
+        my_stone_number = FIRST_COLOR_STONE;
         $('#msg').text('あなたのターンです');
     } else {
         is_my_turn = false;
         my_color = second_color;
         opponent_color = first_color;
+        my_stone_number = SECOND_COLOR_STONE;
         $('#msg').text('相手のターンです');
     }
 
@@ -86,10 +111,21 @@ function clickFirstOrSecondBtn()
         type: 'POST',
         data: {'game_id': game_id, 'turn': turn, '_method': 'POST'},
         success: function(response) {
+            // 指定の色で範囲内を塗りつぶす
+            context.fillStyle = board_color;
+            context.fillRect(0, 0, board_size, board_size);
+
+            // 描画処理
+            drawCanvas(cell_num, cell_num, board_size / cell_num, board_color);
+
             $('#selection').css('display', 'none');
             $('#canvas_wrap').css('display', 'block');
             $('.info').css('display', 'block');
             $('#msg').css('display', 'block');
+
+            if(IS_DEBUG_MODE){
+                $('#debug_info').css('display', 'block');
+            }
         },
         fail: function(response) {
            // ajax失敗時の処理
@@ -123,23 +159,52 @@ function getPriority(game_id){
 
 // 描画処理
 function drawCanvas(row_num, col_num, grid_size, color){
+    initDirectionArray();
     initBoard(row_num, col_num, grid_size, color);
+}
+
+// 方向定数の初期化
+function initDirectionArray(){
+    for(var x = -1; x <= 1; x++){
+        for(var y = -1; y <= 1; y++){
+            if(x == 0 && y == 0)
+            {
+                continue;
+            }
+            direction_array.push(new Direction(x, y));
+        }
+    }
 }
 
 // 盤面初期化
 function initBoard(row_num, col_num, grid_size, color){
+    // 石の配置の初期化
     for(var y = 0; y < row_num; y++){
-            for(var x = 0; x < col_num; x++){
-                    drawGrid(x, y, grid_size, color);
-            }
+        for(var x = 0; x < col_num; x++){
+            board_stone_array[y + x * col_num] = NONE;
+        }
     }
 
-    var grid_size_half = grid_size / 2;
-    drawStone(first_color, grid_size * 4 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * STONE_RATIO);
-    drawStone(first_color, grid_size * 5 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * STONE_RATIO);
-    drawStone(second_color, grid_size * 5 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * STONE_RATIO);
-    drawStone(second_color, grid_size * 4 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * STONE_RATIO);
+    // セルの描画
+    for(var y = 0; y < row_num; y++){
+        for(var x = 0; x < col_num; x++){
+            drawGrid(x, y, grid_size, color);
+        }
+    }
 
+    // 石を配置
+    putStone(3, 3, FIRST_COLOR_STONE);
+    putStone(4, 4, FIRST_COLOR_STONE);
+    putStone(4, 3, SECOND_COLOR_STONE);
+    putStone(3, 4, SECOND_COLOR_STONE);
+
+    // 石を描画
+    // drawStone(first_color, grid_size * 4 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * STONE_RATIO);
+    // drawStone(first_color, grid_size * 5 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * STONE_RATIO);
+    // drawStone(second_color, grid_size * 5 - grid_size_half, grid_size * 4 - grid_size_half, grid_size_half * STONE_RATIO);
+    // drawStone(second_color, grid_size * 4 - grid_size_half, grid_size * 5 - grid_size_half, grid_size_half * STONE_RATIO);
+
+    // 石の数を表示
     setStoneNum(true, 2);
     setStoneNum(false, 2);
 }
@@ -182,19 +247,54 @@ function onClick(e) {
     y = e.clientY - rect.top;
 
     // クリック座標を、石を置く座標に変換
-    var grid_size = board_size / cell_num;
-    var grid_size_half = grid_size / 2;
-    var loc_x = 1 + Math.floor(x / grid_size);
-    var loc_y = 1 + Math.floor(y / grid_size);
+    var loc_x = Math.floor(x / grid_size);
+    var loc_y = Math.floor(y / grid_size);
+
+    // 石が置けるか確認
+    if (!canPut(loc_x, loc_y, my_stone_number)) {
+        return;
+    }
 
     // 石を置く
-    drawStone(my_color, grid_size * loc_x - grid_size_half, grid_size * loc_y - grid_size_half, grid_size_half * STONE_RATIO);
+    putStone(loc_x, loc_y, my_stone_number);
 
-    if (!IS_DEBUG_MODE)
-    {
-        // 相手のターンにする
-        is_my_turn = false;
-        $('#msg').text('相手のターンです');
+    // 相手のターンにする
+    is_my_turn = !is_my_turn;
+    $('#msg').text('相手のターンです');
+    if (!IS_DEBUG_MODE){
+        // // 相手のターンにする
+        // is_my_turn = false;
+        // $('#msg').text('相手のターンです');
+    } else {
+        // デバッグモード 次のターンに相手の石を置けるようにする
+        changeMyColor();
+    }
+
+    // 石の数を数える (デバッグモードだと数がおかしい)
+    var first_color_num = countStones(FIRST_COLOR_STONE);
+    var second_color_num = countStones(SECOND_COLOR_STONE);
+    // if (my_stone_number == FIRST_COLOR_STONE) {
+    //     setStoneNum(true, first_color_num);
+    //     setStoneNum(false, second_color_num);
+    // } else {
+    //     setStoneNum(true, second_color_num);
+    //     setStoneNum(false, first_color_num);
+    // }
+    setStoneNum(true, first_color_num);
+    setStoneNum(false, second_color_num);
+
+    if(IS_DEBUG_MODE){
+        var html_text = "";
+        for(var y = 0; y < cell_num; y++){
+            for(var x = 0; x < cell_num; x++){
+                 html_text += board_stone_array[y + x * cell_num];
+            }
+            html_text += "<br>";
+        }
+        $('#board').html(html_text);
+        $('#is_my_turn').text(is_my_turn);
+        $('#my_stone_number').text(my_stone_number);
+        $('#my_color').text(my_color);
     }
 }
 
@@ -202,4 +302,120 @@ function onClick(e) {
 function setStoneNum(is_my, num){
     var id_name = is_my ? "you_info" : "opponent_info";
     $(`#${id_name} > .stone_num`).text(num);
+}
+
+// デバッグ時のみ有効
+function changeMyColor(){
+    if(my_color == first_color){
+        my_color = second_color;
+        my_stone_number = SECOND_COLOR_STONE;
+    }else{
+        my_color = first_color;
+        my_stone_number = FIRST_COLOR_STONE;
+    }
+}
+
+// 石の数を数える
+function countStones(stone_number){
+    var count = 0;
+    for(var y = 0; y < cell_num; y++){
+        for(var x = 0; x < cell_num; x++){
+            if (board_stone_array[y + x * cell_num] == stone_number) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+// 石を置く
+function putStone(x, y, stone_number){
+    // クリックされた場所に石を置く
+    var put_color = stone_number == FIRST_COLOR_STONE ? first_color : second_color;
+    board_stone_array[y + x * cell_num] = stone_number;
+    drawStone(put_color, grid_size * (x + 1) - grid_size_half, grid_size * (y + 1) - grid_size_half, grid_size_half * STONE_RATIO);
+
+    // --- 各方向について、挟んだ相手の石をひっくり返す ---
+    for(var i = 0; i < direction_array.length; i++)
+    {
+        // ある方向の挟んでいる石の数を取得
+        var turn_over_count = TurnOverCount(x, y, direction_array[i], stone_number);
+        for(var step = 1; step <= turn_over_count; step++)
+        {
+            var now_x = x + step * direction_array[i].d_x;
+            var now_y = y + step * direction_array[i].d_y;
+            board_stone_array[now_y + now_x * cell_num] = stone_number;
+            drawStone(put_color, grid_size * (now_x + 1) - grid_size_half, grid_size * (now_y + 1) - grid_size_half, grid_size_half * STONE_RATIO);
+        }
+    }
+}
+
+// 石が置けるか確認
+function canPut(x, y, stone_number){
+    var put_target = board_stone_array[y + x * cell_num];
+    if (put_target != NONE) {
+        // 既に石が置かれている
+        return false;
+    }
+    // 全方向について、相手の石を挟んでいるかチェックする
+    return watchLine(x, y, stone_number);
+}
+
+// 盤の範囲内であればtrueを返す
+function inBoardRange(x, y){
+    if (x < 0 || x >= cell_num) {
+        return false;
+    }
+    if (y < 0 || y >= cell_num) {
+        return false;
+    }
+    return true;
+}
+
+// 相手の石を挟んでいるかチェックする
+function watchLine(x, y, stone_number){
+    // 各方向について処理
+    var turn_over_count = 0;
+    for(var i = 0; i < direction_array.length; i++)
+    {
+        turn_over_count += TurnOverCount(x, y, direction_array[i], stone_number);
+    }
+    if (turn_over_count == 0)
+    {
+        // 全方向について、相手の石を挟んでいない
+        return false;
+    }
+    // 挟んでいる石がある
+    return true;
+}
+
+// 引数の方向について、挟んでいる相手の石の数を返す
+function TurnOverCount(x, y, d, stone_number)
+{
+    var d_x = d.d_x;
+    var d_y = d.d_y;
+    var step = 1;   // いくつ隣か
+    var exist_opponent_stone = false;   // 相手の石を挟んでいるか
+
+    while (inBoardRange(x + step * d_x, y + step * d_y)) {
+        var location = board_stone_array[(y + step * d_y) + (x + step * d_x) * cell_num];
+        if (location == NONE) {
+            break;
+        }
+        if (exist_opponent_stone) {
+            // 既に相手の石を挟んでいる
+            if (location == stone_number) {
+                // 自分の石だった場合
+                return step - 1;
+            }
+        } else {
+            // まだ相手の石を挟んでいない
+            if (location != stone_number) {
+                // 相手の石だった場合
+                exist_opponent_stone = true;
+            }
+        }
+        step++;
+    }
+    return 0;
 }
